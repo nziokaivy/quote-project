617d182561c1e09728268ab855041efc2d6c949b
nziokaivy <nziokaivy@gmail.com>
2019-01-12 20:29:22 +0300

    add new component vote

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { VoteComponent } from './vote/vote.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
-    QuoteFormComponent
+    QuoteFormComponent,
+    VoteComponent
   ],
   imports: [
     BrowserModule,
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -7,21 +7,28 @@ import { Quote } from '../quotes';
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
+
 export class QuoteComponent implements OnInit { 
+  
   quotes = [
     new Quote(1, "lorem ipsum is real", "Ivy", "Dami"),
     new Quote(2, "lorem ipsum is no lie", "Ivy", "Dami"),
     new Quote(3, "lorem ipsum is lit", "Ivy", "Dami"),
   ]
 
-  showDescription= false;
+  showDescription = false;
+
+  addNewQuote(quote) {
+    this.quotes.push(quote);
+  }
 
   toggleQuote() {
     this.showDescription =! this.showDescription;
   }
   deleteQuote(i) {
-    this.quotes.splice(i,1);
-  }
+      this.quotes.splice(i, 1)
+    }
+    
   constructor() { }
 
   ngOnInit() {

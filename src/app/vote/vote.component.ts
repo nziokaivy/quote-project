67b727d6f5b3a201e99e2ada58d123af192624c5
nziokaivy <nziokaivy@gmail.com>
2019-01-12 20:38:47 +0300

    add quotes class

--- src/app/vote/vote.component.ts
+++ src/app/vote/vote.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-
+import { Quote } from '../quotes';
 @Component({
   selector: 'app-vote',
   templateUrl: './vote.component.html',

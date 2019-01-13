1702c488f5d165944ac926e7ef2c67684a5858b4
nziokaivy <nziokaivy@gmail.com>
2019-01-13 21:51:48 +0300

    add date in the constructor

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,9 +1,12 @@
 export class Quote {
+    showDescription = false;
+    
     constructor(
     public id: number,
     public quote: string,
     public author: string,
-    public submitter: string,) {
+    public submitter: string,
+    public dateComplete: Date) {
 
     }
     

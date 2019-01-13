b83a34422203dafb52c16628eb0d95e908f3537a
nziokaivy <nziokaivy@gmail.com>
2019-01-13 22:09:27 +0300

    add date functionality

--- src/app/date.pipe.ts
+++ src/app/date.pipe.ts
@@ -5,8 +5,23 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DatePipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
-  }
+  transform(value: any): number {
+    
+    let today:Date = new Date(); 
+    let todayWithNoTime:any = new Date(today.getFullYear(), today.getMonth(), today.getDate())
+    var dateDifference =Math.abs(value - todayWithNoTime )
+    const secondsInADay= 86400; 
 
+    var dateDifferenceSeconds = dateDifference * 0.001; 
+
+    var dateCounter = Math.round(dateDifferenceSeconds/secondsInADay);
+
+    if (dateCounter >= 1 && value > todayWithNoTime){
+        
+      return dateCounter;
+    
+    }else{
+        return 0;
+    }
+  }
 }

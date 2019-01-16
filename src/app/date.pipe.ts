01a08dabc5122b8fd14e20de9889e352b6f2f0cb
nziokaivy <nziokaivy@gmail.com>
2019-01-16 19:38:19 +0300

    edit quote form html

--- src/app/date.pipe.ts
+++ src/app/date.pipe.ts
@@ -6,20 +6,17 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class DatePipe implements PipeTransform {
 
   transform(value: any): number {
-    
     let today:Date = new Date(); 
-    let todayWithNoTime:any = new Date(today.getFullYear(), today.getMonth(), today.getDate())
-    var dateDifference =Math.abs(value - todayWithNoTime )
+    let todayWithNoTime:any = new Date(today.getFullYear(),today.getMonth(),today.getDate())
+    var dateDifference =Math.abs(todayWithNoTime-value )
     const secondsInADay= 86400; 
 
-    var dateDifferenceSeconds = dateDifference * 0.001; 
+    var dateDifferenceSeconds=dateDifference*0.001; 
 
-    var dateCounter = Math.round(dateDifferenceSeconds/secondsInADay);
+    var dateCounter = dateDifferenceSeconds/secondsInADay;
 
     if (dateCounter >= 1 && todayWithNoTime > value){
-        
-      return dateCounter;
-    
+        return dateCounter;
     }else{
         return 0;
     }
